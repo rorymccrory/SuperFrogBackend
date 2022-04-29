98fe10c055d157439c28d098990e4f3c932e69b7
talmage123 <talmage123@gmail.com>
2022-04-29 18:31:49 -0500

    CORS

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -15,29 +15,29 @@ public class UserController {
     public UserController(UserService service) {
         this.service = service;
     }
-
+    @CrossOrigin
     @GetMapping
     public Result findAll() {
         return new Result(true, StatusCode.SUCCESS, "Find All Success", service.findAll());
     }
-
+    @CrossOrigin
     @GetMapping("/{id}")
     public Result findById(@PathVariable Integer id) {
         return new Result(true, StatusCode.SUCCESS, "Find One Success", service.findById(id));
     }
-
+    @CrossOrigin
     @PostMapping
     public Result save(@RequestBody User user) {
         service.save(user);
         return new Result(true, StatusCode.SUCCESS, "Save Success");
     }
-
+    @CrossOrigin
     @PutMapping("/{id}")
     public Result update(@RequestBody User user, @PathVariable Integer id) {
         service.update(id, user);
         return new Result(true, StatusCode.SUCCESS, "Update Success");
     }
-
+    @CrossOrigin
     @DeleteMapping("/{id}")
     public Result delete(@PathVariable Integer id) {
         service.deleteById(id);
