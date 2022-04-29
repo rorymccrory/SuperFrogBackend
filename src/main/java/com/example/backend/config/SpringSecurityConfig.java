75fd848033a19f2ac3780c2aaee8d286e1f2349a
talmage123 <talmage123@gmail.com>
2022-04-29 18:34:50 -0500

    Update SpringSecurityConfig.java

--- src/main/java/com/example/backend/config/SpringSecurityConfig.java
+++ src/main/java/com/example/backend/config/SpringSecurityConfig.java
@@ -10,23 +10,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserService userService;
 
+    @CrossOrigin
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
 
+    @CrossOrigin
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //let Spring Security know how to retrieve user info from system
         auth.userDetailsService(this.userService);
     }
-
+    @CrossOrigin
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
